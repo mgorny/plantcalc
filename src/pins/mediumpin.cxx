0b5ea83062faccf40ee7b2ba533811775baac319
Michał Górny <mgorny@gentoo.org>
2012-06-16 21:18:10 +0200

    Remove the Stream API completely.

--- src/pins/mediumpin.cxx
+++ src/pins/mediumpin.cxx
@@ -18,6 +18,36 @@ MediumPin::MediumPin()
 }
 
 MediumPin::MediumPin(double p, double T)
-	: MediumStream(p, T)
+	: _p(p), _T(T)
 {
+}
+
+PosOrUnsetDouble MediumPin::p()
+{
+	return _p;
+}
+
+void MediumPin::p(PosOrUnsetDouble newval)
+{
+	_p = newval;
+}
+
+PosOrUnsetDouble MediumPin::T()
+{
+	return _T;
+}
+
+void MediumPin::T(PosOrUnsetDouble newval)
+{
+	_T = newval;
+}
+
+PosOrUnsetDouble MediumPin::h()
+{
+	return _h;
+}
+
+void MediumPin::h(PosOrUnsetDouble newval)
+{
+	_h = newval;
 }
--- src/pins/mediumpin.hxx
+++ src/pins/mediumpin.hxx
@@ -9,15 +9,24 @@
 #define _PLANTCALC_PINS_MEDIUMPIN_HXX 1
 
 #include "../pin.hxx"
-#include "../streams/mediumstream.hxx"
+#include "../util/posorunsetdouble.hxx"
 
-class MediumPin : public Pin, public MediumStream
+class MediumPin : public Pin
 {
+	PosOrUnsetDouble _p, _T, _h;
+
 public:
 	MediumPin();
 	MediumPin(double p, double T);
 
 	virtual ~MediumPin();
+
+	PosOrUnsetDouble p();
+	void p(PosOrUnsetDouble newval);
+	PosOrUnsetDouble T();
+	void T(PosOrUnsetDouble newval);
+	PosOrUnsetDouble h();
+	void h(PosOrUnsetDouble newval);
 };
 
 #endif /*_PLANTCALC_PINS_MEDIUMPIN_HXX*/
