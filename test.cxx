60636d136f971478c1ef4e05e8a167b59abdb026
Michał Górny <mgorny@gentoo.org>
2012-07-03 20:24:25 +0200

    test: add device/conn iterating code.

--- test.cxx
+++ test.cxx
@@ -7,6 +7,8 @@
 
 #include "src/connections/medium/waterconnection.hxx"
 
+#include <vector>
+
 int main()
 {
 	Boiler b(.9, 10, 773.15);
@@ -14,6 +16,32 @@ int main()
 
 	WaterConnection bc(b.out(), c.in());
 	WaterConnection cb(c.in(), b.out());
+
+	typedef std::vector<Device*> device_list;
+	device_list devices;
+	devices.push_back(&b);
+	devices.push_back(&c);
+
+	typedef std::vector<Connection*> connection_list;
+	connection_list connections;
+	connections.push_back(&bc);
+	connections.push_back(&cb);
+
+	do
+	{
+		for (device_list::iterator it = devices.begin();
+				it != devices.end(); ++it)
+		{
+			Device& d = **it;
+		}
+
+		for (connection_list::iterator it = connections.begin();
+				it != connections.end(); ++it)
+		{
+			Connection& c = **it;
+		}
+	}
+	while (0);
 
 	return 0;
 }
