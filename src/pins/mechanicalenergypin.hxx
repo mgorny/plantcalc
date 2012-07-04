5cb0c444f532d3794a71e914a02eaeb5b00842ca
Michał Górny <mgorny@gentoo.org>
2012-07-04 20:38:57 +0200

    Introduce MechanicalEnergyPin.

---
+++ src/pins/mechanicalenergypin.hxx
@@ -0,0 +1,25 @@
+/* plantcalc -- Power plant modelling
+ * (c) 2012 Michał Górny
+ * Released under the terms of the 2-clause BSD license
+ */
+
+#pragma once
+
+#ifndef _PLANTCALC_PINS_MECHANICALENERGYPIN_HXX
+#define _PLANTCALC_PINS_MECHANICALENERGYPIN_HXX 1
+
+#include "../pin.hxx"
+#include "../variable.hxx"
+
+class MechanicalEnergyPin : public Pin
+{
+	int _id;
+	Variable _P;
+
+public:
+	MechanicalEnergyPin();
+
+	Variable& P();
+};
+
+#endif /*_PLANTCALC_PINS_MECHANICALENERGYPIN_HXX*/
