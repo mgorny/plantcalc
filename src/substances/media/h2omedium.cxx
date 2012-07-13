436d3bf6554039387a53a03ff792bbdd5bd22fd1
Michał Górny <mgorny@gentoo.org>
2012-07-13 11:35:05 +0200

    H2OMedium.solve(): handle overdetermined equations.

--- src/substances/media/h2omedium.cxx
+++ src/substances/media/h2omedium.cxx
@@ -10,6 +10,7 @@
 #include "h2omedium.hxx"
 #include "../../exceptions/contradictionerror.hxx"
 
+#include <cmath>
 #include <stdexcept>
 
 #include <h2o>
@@ -23,39 +24,66 @@ bool H2OMedium::solve(Variable& p, Variable& T,
 			Variable& h, Variable& s, Variable& x,
 			double epsilon)
 {
-	int count = p.is_set() + T.is_set() + h.is_set()
-		+ s.is_set() + x.is_set();
-
-	if (count > 2) // XXX: handle epsilon comparisons
-		throw ContradictionError();
-	else if (count < 2)
-		return false;
-
 	h2o::H2O state;
+	bool used_p = false, used_T = false, used_h = false,
+		used_s = false, used_x = false;
 
-	if (p.is_set() && T.is_set())
-		state = h2o::H2O::pT(p, T);
-	else if (p.is_set() && h.is_set())
-		state = h2o::H2O::ph(p, h);
-	else if (p.is_set() && s.is_set())
-		state = h2o::H2O::ps(p, s);
-	else if (p.is_set() && x.is_set())
-		state = h2o::H2O::px(p, x);
+	if (p.is_set())
+	{
+		used_p = true;
+
+		if (T.is_set())
+		{
+			used_T = true;
+			state = h2o::H2O::pT(p, T);
+		}
+		else if (h.is_set())
+		{
+			used_h = true;
+			state = h2o::H2O::ph(p, h);
+		}
+		else if (s.is_set())
+		{
+			used_s = true;
+			state = h2o::H2O::ps(p, s);
+		}
+		else if (x.is_set())
+		{
+			used_x = true;
+			state = h2o::H2O::px(p, x);
+		}
+		else
+			return false;
+	}
 	else if (T.is_set() && x.is_set())
+	{
+		used_T = true;
+		used_x = true;
 		state = h2o::H2O::Tx(T, x);
+	}
 	else
-		throw std::runtime_error("Unsupported water state equation arguments.");
+		return false;
 
 	if (!p.is_set())
 		p.set_value(state.p());
+	else if (!used_p && std::abs(state.p() - p) >= epsilon)
+		throw ContradictionError();
 	if (!T.is_set())
 		T.set_value(state.T());
+	else if (!used_T && std::abs(state.T() - T) >= epsilon)
+		throw ContradictionError();
 	if (!h.is_set())
 		h.set_value(state.h());
+	else if (!used_h && std::abs(state.h() - h) >= epsilon)
+		throw ContradictionError();
 	if (!s.is_set())
 		s.set_value(state.s());
+	else if (!used_s && std::abs(state.s() - s) >= epsilon)
+		throw ContradictionError();
 	if (!x.is_set()) // XXX: supercritical?
 		x.set_value(state.x());
+	else if (!used_x && std::abs(state.x() - x) >= epsilon)
+		throw ContradictionError();
 
 	return true;
 }
