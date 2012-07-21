db8eae635d7b1163743444d91c83649c698a0a4e
Michał Górny <mgorny@gentoo.org>
2012-07-21 15:43:31 +0200

    Doc: Substance.

--- src/substance.hxx
+++ src/substance.hxx
@@ -8,11 +8,27 @@
 #ifndef _PLANTCALC_SUBSTANCE_HXX
 #define _PLANTCALC_SUBSTANCE_HXX 1
 
+/**
+ * A definition of particular substance which can flow through
+ * the system.
+ *
+ * The Substance class represents the model of a particular substance
+ * which could be used in an appropriate Connection type (passed to
+ * Connection::substance()).
+ *
+ * A substance is usually used to define additional equations which
+ * correlate the properties of substance flowing through the connection.
+ * Thus, the classes derived from Substance provide appropriate equation
+ * solving functions.
+ */
 class Substance
 {
 public:
 	virtual ~Substance();
 
+	/**
+	 * Obtain the textual name of the substance.
+	 */
 	virtual const char* name() const = 0;
 };
 
