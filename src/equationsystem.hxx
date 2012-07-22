60d734a81d4b89740fce478c1229f26e90c84dce
Michał Górny <mgorny@gentoo.org>
2012-07-22 16:01:28 +0200

    Doc: EquationSystem.

--- src/equationsystem.hxx
+++ src/equationsystem.hxx
@@ -13,14 +13,34 @@
 #include <vector>
 #include <ostream>
 
+/**
+ * A system of equations.
+ *
+ * The EquationSystem class stores a list of Equation pointers which
+ * form a system of equations. Such a system can be subject to solving
+ * through EquationSolver derived classes.
+ *
+ * The Equation pointers will not be owned or freed
+ * by the EquationSystem. The caller needs to ensure their persistence
+ * throughout EquationSystem lifetime.
+ */
 class EquationSystem : public std::vector<Equation*>
 {
 public:
+	/**
+	 * Instantiate a new, empty system of equations.
+	 */
 	EquationSystem();
 
+	/**
+	 * Append the equations in system @a rhs to the system.
+	 */
 	EquationSystem& operator+=(const EquationSystem& rhs);
 };
 
+/**
+ * Print the system of equations into stream @a f.
+ */
 std::ostream& operator <<(std::ostream& f, const EquationSystem& eqs);
 
 #endif /*_PLANTCALC_EQUATIONSYSTEM_HXX*/
